58ed591ea2044cfbc5507f5e4d05129f7f9d3913
unknown <Dianaflochacon456@gmail.com>
2025-06-04 16:53:57 -0400

    dockerizacion

--- red-social-academica/src/test/java/red_social_academica/red_social_academica/RedSocialAcademicaApplicationTests.java
+++ red-social-academica/src/test/java/red_social_academica/red_social_academica/RedSocialAcademicaApplicationTests.java
@@ -2,8 +2,10 @@ package red_social_academica.red_social_academica;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RedSocialAcademicaApplicationTests {
 
 	@Test
